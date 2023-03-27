56131c3cc63e64b2b1ef94634a7d2aa3cfe76fbe
Waqas Tahir <wakaztahir@gmail.com>
2023-03-27 22:13:49 +0500

    fix for primitive char and string

--- kte/src/commonMain/kotlin/com/wakaztahir/kte/parser/ParseRuntime.kt
+++ kte/src/commonMain/kotlin/com/wakaztahir/kte/parser/ParseRuntime.kt
@@ -6,6 +6,7 @@ import com.wakaztahir.kte.model.LazyBlock
 import com.wakaztahir.kte.model.StringValue
 import com.wakaztahir.kte.model.model.ReferencedValue
 import com.wakaztahir.kte.parser.stream.DestinationStream
+import com.wakaztahir.kte.parser.stream.SourceStream
 import com.wakaztahir.kte.parser.stream.increment
 import kotlin.jvm.JvmInline
 
@@ -32,18 +33,30 @@ private value class WriteString(val string: ReferencedValue) : CodeGen {
     }
 }
 
+private fun SourceStream.parseRefCharValue() : ReferencedValue? {
+    parseReferencedValue()?.let { return it }
+    parseCharacterValue()?.let { return it }
+    return null
+}
+
+private fun SourceStream.parseRefStringValue() : ReferencedValue? {
+    parseReferencedValue()?.let { return it }
+    parseStringValue()?.let { return it }
+    return null
+}
+
 fun LazyBlock.parseRuntimeGen(): CodeGen? {
     if (source.currentChar == '@') {
         if (source.increment(CHAR_DIRECTIVE)) {
             if (!source.increment('(')) throw IllegalStateException("expected '(' got ${source.currentChar}")
             val value =
-                source.parseReferencedValue() ?: throw IllegalStateException("value for runtime directive not found")
+                source.parseRefCharValue() ?: throw IllegalStateException("value for runtime directive not found")
             if (!source.increment(')')) throw IllegalStateException("expected ')' got ${source.currentChar}")
             return WriteChar(value)
         } else if (source.increment(STRING_DIRECTIVE)) {
             if (!source.increment('(')) throw IllegalStateException("expected '(' got ${source.currentChar}")
             val value =
-                source.parseReferencedValue() ?: throw IllegalStateException("value for runtime directive not found")
+                source.parseRefStringValue() ?: throw IllegalStateException("value for runtime directive not found")
             if (!source.increment(')')) throw IllegalStateException("expected ')' got ${source.currentChar}")
             return WriteString(value)
         }
