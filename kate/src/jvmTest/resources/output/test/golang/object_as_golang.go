08920b1e2a5527c7f81072638d72d1922c06fa22
Waqas Tahir <wakaztahir@gmail.com>
2023-03-31 23:39:05 +0500

    double as float64

--- kate/src/jvmTest/resources/output/test/golang/object_as_golang.go
+++ kate/src/jvmTest/resources/output/test/golang/object_as_golang.go
@@ -2,22 +2,22 @@ package object_as_struct
 
 type NestedObject struct {
 	testList []int
-	testMList []float32
+	testMList []float64
 	testBoolean bool
 	testChar rune
 	testVariable string
-	testDouble float32
+	testDouble float64
 	testLong int64
 }
 
 type TestObject struct {
 	nestedObject NestedObject
 	testList []int
-	testMList []float32
+	testMList []float64
 	testBoolean bool
 	testChar rune
 	testVariable string
 	testListList [][]int
-	testDouble float32
+	testDouble float64
 	testLong int64
 }
